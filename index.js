d4862f1379abecaefeb42f24313732e659a53f59
Hexagon <hexagon@56k.guru>
2019-09-24 21:19:48 +0100

    Minor fix

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-var Decent = require("./lib/decent.js");
+var Decent = require("./lib/decent.js"),
+	blessed = require('blessed'),
+	contrib = require('blessed-contrib'),
+	screen = blessed.screen({smartCSR: true}),
+	grid, map, log,
+	d;
 
-var blessed = require('blessed')
- , contrib = require('blessed-contrib')
- , screen = blessed.screen({smartCSR: true})
-  , grid = new contrib.grid({rows: 1, cols: 3, screen: screen});
+function doLog(msg) {
+	log.log(msg);
+	screen.render();
+}
 
-   var map = grid.set(0, 0, 1, 2, contrib.map, {label: 'Nodes Location'});
-map.addMarker({"lon" : "16.0000", "lat" : "65.4000", color: "red", char: "X" });
+grid = new contrib.grid({rows: 1, cols: 3, screen: screen});
 
+map = grid.set(0, 0, 1, 2, contrib.map, {
+	label: 'Nodes Location'
+});
 
-   var log = grid.set(0, 2, 1, 1, contrib.log,contrib.log,
-  { fg: "green"
-  , selectedFg: "green"
-  , label: 'Server Log'});
+map.addMarker({
+	"lon" : "16.0000",
+	"lat" : "65.4000",
+	color: "red",
+	char: "X" 
+});
+
+log = grid.set(0, 2, 1, 1, contrib.log, { 
+	fg: "green",
+	selectedFg: "green",
+	label: 'Server Log'
+});
 
 log.log("GUI Initiated");
 
-
-var d = new Decent(512, 10, "1,3,3.7",false);
+d = new Decent(512, 10, "1,3,3.7",false);
 d.locate();
 
 d.events.on('repl',(node,messageType) => doLog("REPL:"+node.toString()+">"+messageType));
@@ -31,10 +45,5 @@ d.events.on('listening', (port) => doLog("Listening at "+port));
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
  return process.exit(0);
 });
-
-function doLog(msg) {
-	log.log(msg);
-	screen.render();
-}
 
 screen.render();

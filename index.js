9f2dae92c2395cd79d00485da9ddae4856ae669b
Hexagon <hexagon@56k.guru>
2019-09-24 21:15:20 +0100

    Mess in progress

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 var Decent = require("./lib/decent.js");
 
-console.log('Decent \"SERVER\" is starting up!');
+var blessed = require('blessed')
+ , contrib = require('blessed-contrib')
+ , screen = blessed.screen({smartCSR: true})
+  , grid = new contrib.grid({rows: 1, cols: 3, screen: screen});
+
+   var map = grid.set(0, 0, 1, 2, contrib.map, {label: 'Nodes Location'});
+map.addMarker({"lon" : "16.0000", "lat" : "65.4000", color: "red", char: "X" });
+
+
+   var log = grid.set(0, 2, 1, 1, contrib.log,contrib.log,
+  { fg: "green"
+  , selectedFg: "green"
+  , label: 'Server Log'});
+
+log.log("GUI Initiated");
+
+
 var d = new Decent(512, 10, "1,3,3.7",false);
 d.locate();
+
+d.events.on('repl',(node,messageType) => doLog("REPL:"+node.toString()+">"+messageType));
+d.events.on('send',(node,messageType) => doLog("SEND:"+node+">"+messageType));
+d.events.on('state',(prevState,curState) => doLog("State changed:"+prevState+">"+curState));
+d.events.on('roaming',(goal,at) => doLog("Roaming: GOAL: "+goal+", at: "+at));
+d.events.on('recv', (node,messageType) => doLog("REPL:"+node.toString()+">"+messageType));
+d.events.on('error', (err) => doLog("ERR:"+err));
+d.events.on('listening', (port) => doLog("Listening at "+port));
+
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+ return process.exit(0);
+});
+
+function doLog(msg) {
+	log.log(msg);
+	screen.render();
+}
+
+screen.render();
